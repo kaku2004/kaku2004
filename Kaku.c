03c38407b7888e9c61aba1199a3603412e916fc4
kaku2004 <145787606+kaku2004@users.noreply.github.com>
2023-11-30 16:59:37 +0530

    Update Kaku.c

--- Kaku.c
+++ Kaku.c
@@ -1,1 +1,22 @@
-i am kaku 
+/* Find smaller of two numbers */
+
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter the two numbers:");
+  scanf("%d%d",&a&b);
+  if(a>b)
+{
+printf("Smaller number is b");
+}
+else if(a==b)
+{
+printf("The two numbers are equal");
+}
+else
+{ 
+printf("The smaller number is a");
+}
+return 1;
+}
